dd8fd09d87c968c458729f7a0f0643a4e5578183
Pushkar Anand <anandpushkar088@gmail.com>
2018-09-02 02:34:06 +0530

    added codechef creds.

--- includes/define.php
+++ includes/define.php
@@ -6,3 +6,12 @@ define('FROM_EMAIL', 'anandpushkar088@gmail.com');
 define('SUBJECT_CRITICAL', 'Critical Error');
 define('SUBJECT_ALERT', 'New Alert');
 define('SUBJECT_EMERGENCY', 'EMERGENCY');
+
+//DATABASE
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'codechef_user');
+define('DB_PASSWORD', 'm3Jy7^^l*C2GzE0H7wM2');
+define('DB_NAME', 'codechef');
+
+define('CODECHEF_CLIENT_ID', '8777baaa531e12d69cc961b344c5ab2a');
+define('CODECHEF_CLIENT_SECRET', '081e091ff86ea198ea9e73b175670170');
