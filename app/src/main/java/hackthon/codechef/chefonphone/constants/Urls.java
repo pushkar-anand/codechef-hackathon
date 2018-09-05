c41c9334b71c5056bb3dc0ab798e90c96c65e98a
Pushkar Anand <anandpushkar088@gmail.com>
2018-09-05 15:55:15 +0530

    URL.

--- app/src/main/java/hackthon/codechef/chefonphone/constants/Urls.java
+++ app/src/main/java/hackthon/codechef/chefonphone/constants/Urls.java
@@ -4,11 +4,14 @@ public class Urls {
 
     private static final String BASE_URL = "https://codechef-hackathon.pushkaranand.me";
     public static final String LOGIN_URL = BASE_URL + "/login";
+
     public static final String CONTEST_URL_SHORT_LIST = BASE_URL + "/contest/list/short";
-    public static final String CONTEST_URL_LONG = BASE_URL + "/contest/list/short";
+    public static final String CONTEST_URL_LONG = BASE_URL + "/contest/list/long";
     public static final String RECOMMEND_URL = BASE_URL + "/recommend";
 
     public static final String PRACTICE_BASE_URL = BASE_URL + "/practice";
-    public static final String CONTEST_DETAILS_URL = "/contest/problems/list";
+    public static final String CONTEST_DETAILS_URL = BASE_URL + "/contest/problems/list";
+
+    public static final String CONTEST_PROBLEM_LOADER = BASE_URL + "/contest/problem/details";
 
 }
