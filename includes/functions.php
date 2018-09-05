5abec98a73ac138fb2d9bdcfdf6bb9a834e67a98
Pushkar Anand <anandpushkar088@gmail.com>
2018-09-05 12:04:20 +0530

    fixes.

--- includes/functions.php
+++ includes/functions.php
@@ -8,11 +8,9 @@
  function verify_hash(string $hash):bool
  {
      $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-     error_log($url);
+     
      $rm = "&hash=$hash";
-
      $url = str_replace($rm, '', $url);
-
 
      $calc_hash = hash('sha256', $url . API_SECRET);
      return $calc_hash == $hash;
