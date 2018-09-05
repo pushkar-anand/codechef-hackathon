061ca027d543dc5448501e3dd34be4c8fc927a3a
Pushkar Anand <anandpushkar088@gmail.com>
2018-09-05 12:30:30 +0530

    change in file structure.

---
+++ controllers/contest/problems.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pushkar
+ * Date: 5/9/18
+ * Time: 12:28 PM
+ */
--- routes/route.php
+++ routes/route.php
@@ -9,20 +9,22 @@ $route = new Route();
 
 try {
 
-    $route->addMatch('GET', '/login', __DIR__ . '/../controllers/login.php');
-    $route->addMatch('GET', '/login/redirect', __DIR__ . '/../controllers/login.redirect.php');
+    $route->addMatch('GET', '/login', __DIR__ . '/../controllers/login/login.php');
+    $route->addMatch('GET', '/login/redirect', __DIR__ . '/../controllers/login/redirect.php');
 
-    $route->addMatch('GET', '/contest/list/short', __DIR__ . '/../controllers/contest.list.short.php');
-    $route->addMatch('GET', '/contest/list/all', __DIR__ . '/../controllers/contest.list.all.php');
+    $route->addMatch('GET', '/contest/list/short', __DIR__ . '/../controllers/contest/list.short.php');
+    $route->addMatch('GET', '/contest/list/all', __DIR__ . '/../controllers/contest/list.all.php');
+    $route->addMatch('GET', '/contest/problems', __DIR__ . '/../controllers/contest/problems.php');
+    $route->addMatch('GET', '/contest/problem/details', __DIR__ . '/../controllers/contest/problem.detail.php');
 
     $route->addMatch('GET', '/recommend', __DIR__ . '/../controllers/recommend.php');
 
-    $route->addMatch('GET', '/practice/beginner', __DIR__ . '/../controllers/practice.beginner.php');
-    $route->addMatch('GET', '/practice/easy', __DIR__ . '/../controllers/practice.easy.php');
-    $route->addMatch('GET', '/practice/medium', __DIR__ . '/../controllers/practice.medium.php');
-    $route->addMatch('GET', '/practice/hard', __DIR__ . '/../controllers/practice.hard.php');
-    $route->addMatch('GET', '/practice/challenge', __DIR__ . '/../controllers/practice.challenge.php');
-    $route->addMatch('GET', '/practice/peer', __DIR__ . '/../controllers/practice.peer.php');
+    $route->addMatch('GET', '/practice/beginner', __DIR__ . '/../controllers/practice/beginner.php');
+    $route->addMatch('GET', '/practice/easy', __DIR__ . '/../controllers/practice/easy.php');
+    $route->addMatch('GET', '/practice/medium', __DIR__ . '/../controllers/practice/medium.php');
+    $route->addMatch('GET', '/practice/hard', __DIR__ . '/../controllers/practice/hard.php');
+    $route->addMatch('GET', '/practice/challenge', __DIR__ . '/../controllers/practice/challenge.php');
+    $route->addMatch('GET', '/practice/peer', __DIR__ . '/../controllers/practice/peer.php');
 
     $route->addMatch('GET', '/problem/details', __DIR__ . '/../controllers/problems.details.php');
 
