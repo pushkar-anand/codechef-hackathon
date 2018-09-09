27b9a5eae9e5d8f2e73dfcaab53db94a3068bbac
Pushkar Anand <anandpushkar088@gmail.com>
2018-09-09 15:20:08 +0530

    finished editor code and options.

--- app/src/main/java/hackthon/codechef/chefonphone/activities/IDEActivity.java
+++ app/src/main/java/hackthon/codechef/chefonphone/activities/IDEActivity.java
@@ -11,6 +11,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -64,8 +65,8 @@ public class IDEActivity extends AppCompatActivity
             }
 
             @JavascriptInterface
-            public void testMethod() {
-
+            public void logToLogcat(String msg) {
+                Log.d("Js console: ", msg);
             }
 
         }
