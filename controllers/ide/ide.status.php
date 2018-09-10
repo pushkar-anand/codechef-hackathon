d6508b41011a5bb108005be2feeb3d1de5d18c25
Pushkar Anand <anandpushkar088@gmail.com>
2018-09-10 22:18:32 +0530

    wrote code for /ide/status.

--- controllers/ide/ide.status.php
+++ controllers/ide/ide.status.php
@@ -1,7 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: pushkar
- * Date: 10/9/18
- * Time: 10:12 PM
- */
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use PhpUseful\Functions;
+
+if (isset($_GET['token']) && isset($_GET['user']) && isset($_GET['status']) && isset($_GET['hash'])) {
+
+    $status = $_GET['status'];
+
+    $hash = $_GET['hash'];
+    if (verify_hash($hash)) {
+
+        $token = User::getCodechefToken($_GET['token'], Functions::escape_input($_GET['user']));
+        if ($token !== false) {
+
+            $api_url = CODECHEF_API_BASE_URL . "/ide/status?link=$status";
+
+            $apiRequest = new CodechefApiCall($token, $api_url);
+            $apiRequest->execute();
+            $result = $apiRequest->getResult();
+            error_log($result);
+            var_dump($result);
+
+        } else {
+            \PhpUseful\EasyHeaders::bad_request();
+        }
+    } else {
+        \PhpUseful\EasyHeaders::bad_request();
+    }
+} else {
+    \PhpUseful\EasyHeaders::bad_request();
+}
