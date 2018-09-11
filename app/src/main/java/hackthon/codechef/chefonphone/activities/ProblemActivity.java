ef9e8c4c3dec1966069c2c217a99be8a1eb2bf49
Pushkar Anand <anandpushkar088@gmail.com>
2018-09-11 23:33:19 +0530

    added problem redirects.

--- app/src/main/java/hackthon/codechef/chefonphone/activities/ProblemActivity.java
+++ app/src/main/java/hackthon/codechef/chefonphone/activities/ProblemActivity.java
@@ -1,5 +1,6 @@
 package hackthon.codechef.chefonphone.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -11,5 +12,7 @@ public class ProblemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_problem);
+
+        Intent intent = getIntent();
     }
 }
