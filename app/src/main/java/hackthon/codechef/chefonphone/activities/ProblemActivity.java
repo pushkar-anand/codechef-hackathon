aaa19a56ab40d95be1941a3769106d6024c3da1b
Pushkar Anand <anandpushkar088@gmail.com>
2018-09-12 00:15:56 +0530

    wrote code to hide loader and show data view.

--- app/src/main/java/hackthon/codechef/chefonphone/activities/ProblemActivity.java
+++ app/src/main/java/hackthon/codechef/chefonphone/activities/ProblemActivity.java
@@ -7,6 +7,8 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import hackthon.codechef.chefonphone.R;
 import hackthon.codechef.chefonphone.asyncloaders.ContestProblemsDetailsLoader;
@@ -16,6 +18,8 @@ import hackthon.codechef.chefonphone.data.Problem;
 public class ProblemActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Problem> {
 
     private String contestCode = null, problemCode;
+    private ProgressBar problemDetailsLoader;
+    private View includeProblemView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,10 +34,16 @@ public class ProblemActivity extends AppCompatActivity implements LoaderManager.
             contestCode = intent.getStringExtra(StringKeys.PROBLEM_ACTIVITY_CONTEST_CODE);
         }
 
+        problemDetailsLoader = findViewById(R.id.problemDetailsLoader);
+        includeProblemView = findViewById(R.id.problemViewInclude);
+
     }
 
     private void updateViewWithProblemData(Problem problem) {
 
+
+        problemDetailsLoader.setVisibility(View.GONE);
+        includeProblemView.setVisibility(View.VISIBLE);
     }
 
     @NonNull
