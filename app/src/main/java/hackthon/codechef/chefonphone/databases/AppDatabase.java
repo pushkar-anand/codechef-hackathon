2e5d5080613c73b35de418e2343ae9ea950adb39
Pushkar Anand <anandpushkar088@gmail.com>
2018-09-12 17:56:47 +0530

    added new table for alarms.

--- app/src/main/java/hackthon/codechef/chefonphone/databases/AppDatabase.java
+++ app/src/main/java/hackthon/codechef/chefonphone/databases/AppDatabase.java
@@ -6,6 +6,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+import android.util.Pair;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -15,15 +16,17 @@ public class AppDatabase extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "codechef.db";
     private static final int DATABASE_VERSION = 1;
-    private static AppDatabase sInstance;
-
-    private static String COMPILE_LOG_TABLE = "compile_logs";
-
-    public static String COMPILE_LOG_TABLE_COLUMN_ID = "id";
     public static String COMPILE_LOG_TABLE_COLUMN_LANG = "language";
     public static String COMPILE_LOG_TABLE_COLUMN_PROBLEM = "problem";
     public static String COMPILE_LOG_TABLE_COLUMN_STATUS = "status";
     public static String COMPILE_LOG_TABLE_COLUMN_TIMESTAMP = "time";
+    public static String ALARMS_TABLE_COLUMN_ID = "id";
+    public static String ALARMS_TABLE_COLUMN_CONTEST = "contest";
+    public static String ALARMS_TABLE_COLUMN_TIME = "remind";
+    private static AppDatabase sInstance;
+    private static String COMPILE_LOG_TABLE = "compile_logs";
+    private static String COMPILE_LOG_TABLE_COLUMN_ID = "id";
+    private static String ALARMS_TABLE = "compile_logs";
 
 
     private AppDatabase(Context context) {
@@ -55,12 +58,24 @@ public class AppDatabase extends SQLiteOpenHelper {
         Log.d("SQL_CREATE: ", createTableQuery);
         sqLiteDatabase.execSQL(createTableQuery);
 
+        createTableQuery = "CREATE TABLE IF NOT EXISTS " + ALARMS_TABLE + "(";
+        createTableQuery += ALARMS_TABLE_COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,";
+        createTableQuery += ALARMS_TABLE_COLUMN_CONTEST + " TEXT NOT NULL";
+        createTableQuery += ALARMS_TABLE_COLUMN_TIME + " TEXT NOT NULL";
+        createTableQuery += ")";
+
+        Log.d("SQL_CREATE: ", createTableQuery);
+        sqLiteDatabase.execSQL(createTableQuery);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
         String deleteQuery = "DROP TABLE IF EXISTS " + COMPILE_LOG_TABLE;
+        sqLiteDatabase.execSQL(deleteQuery);
+
+        deleteQuery = "DROP TABLE IF EXISTS " + ALARMS_TABLE;
         sqLiteDatabase.execSQL(deleteQuery);
 
         onCreate(sqLiteDatabase);
@@ -107,5 +122,48 @@ public class AppDatabase extends SQLiteOpenHelper {
     public void clearCompilationLogs() {
         SQLiteDatabase database = this.getWritableDatabase();
         database.delete(COMPILE_LOG_TABLE, null, null);
+    }
+
+    public void newAlarm(String contest, String time) {
+        SQLiteDatabase database = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(ALARMS_TABLE_COLUMN_CONTEST, contest);
+        contentValues.put(ALARMS_TABLE_COLUMN_TIME, time);
+
+        database.insert(COMPILE_LOG_TABLE, null, contentValues);
+    }
+
+    public ArrayList<Pair<String, String>> getAlarmsData() {
+        ArrayList<Pair<String, String>> pairs = new ArrayList<>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "SELECT * FROM " + ALARMS_TABLE;
+        Cursor res = db.rawQuery(query, null);
+        res.moveToFirst();
+
+        while (!res.isAfterLast()) {
+
+            pairs.add(Pair
+                    .create(res.getString(res.getColumnIndex(ALARMS_TABLE_COLUMN_CONTEST)),
+                            res.getString(res.getColumnIndex(ALARMS_TABLE_COLUMN_TIME))
+                    )
+            );
+            res.moveToNext();
+        }
+        res.close();
+        return pairs;
+    }
+
+    public Integer removeAlarm(String contest) {
+        SQLiteDatabase database = this.getWritableDatabase();
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(ALARMS_TABLE, ALARMS_TABLE_COLUMN_CONTEST + " = ?", new String[]{contest});
+    }
+
+    public void removeAllAlarms() {
+        SQLiteDatabase database = this.getWritableDatabase();
+        database.delete(ALARMS_TABLE, null, null);
     }
 }
