5be162a9c1250540ad3ae544a9b4b5f85540c545
Pushkar Anand <anandpushkar088@gmail.com>
2018-09-12 12:39:44 +0530

    added new keys.

--- app/src/main/java/hackthon/codechef/chefonphone/constants/SharedPrefKeys.java
+++ app/src/main/java/hackthon/codechef/chefonphone/constants/SharedPrefKeys.java
@@ -3,8 +3,11 @@ package hackthon.codechef.chefonphone.constants;
 public class SharedPrefKeys {
 
     public static final String LOGIN_PREF = "login";
+
     public static final String LOGIN_KEY = "ACCESS_KEY";
     public static final String FULLNAME = "NAME";
     public static final String CODECHEF_HANDLE = "CODECHEF_HANDLE";
     public static final String IDE_AUTOSAVE = "lang";
+
+    public static final String ALARM_PREF = "alarms";
 }
