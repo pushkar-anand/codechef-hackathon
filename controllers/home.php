9ea1edbec97a9717b7480c3d0de6d149b6ca3596
Pushkar Anand <anandpushkar088@gmail.com>
2018-09-16 16:08:12 +0530

    added template for homepage.

--- controllers/home.php
+++ controllers/home.php
@@ -2,7 +2,9 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $m = new Mustache_Engine(array(
-    'loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/../../views'),
+    'loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/../views'),
 ));
 
-echo $m->render('home');
+$apk_url = "";
+
+echo $m->render('home', array('apk_url' => $apk_url));
--- logs/initiateLogger.php
+++ logs/initiateLogger.php
@@ -2,8 +2,8 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Monolog\Handler\NativeMailerHandler;
-use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 
 global $log;
 
