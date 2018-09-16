8d033e5ab54a5153c38501b5c51b72babd7c7969
Pushkar Anand <anandpushkar088@gmail.com>
2018-09-16 17:54:41 +0530

    refactor.

--- controllers/login/login.php
+++ controllers/login/login.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/../../vendor/autoload.php';
+
 use PhpUseful\Functions;
 
 $m = new Mustache_Engine(array(
@@ -10,4 +11,4 @@ $state = Functions::random_string(10);
 $_SESSION['state'] = $state;
 $url = 'https://api.codechef.com/oauth/authorize?response_type=code&client_id=' . CODECHEF_CLIENT_ID . '&state=' . $state . '&redirect_uri=' . REDIRECT_URI;
 
-echo $m->render('start', array('url' => $url));
+echo $m->render('login', array('url' => $url));
