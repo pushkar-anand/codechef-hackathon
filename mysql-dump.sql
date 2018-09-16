c1d87744bfd9d6bb1b1896df990450c10cfdbdc2
Pushkar Anand <anandpushkar088@gmail.com>
2018-09-16 11:41:48 +0530

    dump fix.

--- mysql-dump.sql
+++ mysql-dump.sql
@@ -5,14 +5,17 @@ CREATE DATABASE IF NOT EXISTS codechef;
 USE codechef;
 
 /*users table */
-CREATE TABLE IF NOT EXISTS users(id                INT(11) AUTO_INCREMENT PRIMARY KEY,
-                                 name              VARCHAR(50)  NOT NULL,
-                                 codechef_username VARCHAR(50)  NOT NULL,
-                                 codechef_token    VARCHAR(200) NOT NULL,
-                                 refresh_token     VARCHAR(200) NOT NULL,
-                                 token_expire      VARCHAR(100) NOT NULL,
-                                 login_token       VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS users (
+  id                INT(11) AUTO_INCREMENT PRIMARY KEY,
+  name              VARCHAR(50)  NOT NULL,
+  codechef_username VARCHAR(50)  NOT NULL,
+  codechef_token    VARCHAR(200) NOT NULL,
+  refresh_token     VARCHAR(200) NOT NULL,
+  token_expire      VARCHAR(100) NOT NULL,
+  login_token       VARCHAR(256) NOT NULL
+);
 
-ALTER TABLE `users` ADD UNIQUE( `codechef_username`);
+ALTER TABLE `users`
+  ADD UNIQUE (`codechef_username`);
 
 
