6779d914b3545a286311101cd065c20aba054c00
Pushkar Anand <anandpushkar088@gmail.com>
2018-09-23 19:31:18 +0530

    url update.

--- app/src/main/java/hackthon/codechef/chefonphone/constants/Urls.java
+++ app/src/main/java/hackthon/codechef/chefonphone/constants/Urls.java
@@ -3,7 +3,7 @@ package hackthon.codechef.chefonphone.constants;
 public class Urls {
 
     private static final String CODECHEF_HOME = "https://www.codechef.com";
-    private static final String BASE_URL = "https://codechef-hackathon.pushkaranand.me";
+    private static final String BASE_URL = "https://chefonphone.pushkaranand.me";
     public static final String LOGIN_URL = BASE_URL + "/login";
 
     public static final String CONTEST_URL_SHORT_LIST = BASE_URL + "/contest/list/short";
