0d8e03ba7a8637f642f1aa90340efff6f3db4193
Pushkar Anand <anandpushkar088@gmail.com>
2018-09-23 16:19:00 +0530

    added apk.

--- controllers/home.php
+++ controllers/home.php
@@ -5,6 +5,6 @@ $m = new Mustache_Engine(array(
     'loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/../views'),
 ));
 
-$apk_url = "";
+$apk_url = "/apk/chefonphone.apk";
 
 echo $m->render('home', array('apk_url' => $apk_url));
